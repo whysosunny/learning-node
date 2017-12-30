2afead201fd51635f45873a6421d55fcadbc4390
Bharadwaj Pedduri <sunny.bwaj@gmail.com>
2017-12-30 09:27:37 +0530

    completed async basics

---
+++ async/async-basics.js
@@ -0,0 +1,8 @@
+setTimeout(()=> {console.log("Peekaboo!")}, 2000);
+
+setTimeout(()=> {console.log("0 timeout what?!")}, 0);
+
+console.log("Hai re!");
+
+//Learned about Call Stack and the Event Loop
+
