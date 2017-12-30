afd2a4b8f9b80fb96361ec3d33f6f844b5dbf32f
Bharadwaj Pedduri <sunny.bwaj@gmail.com>
2017-12-30 15:43:48 +0530

    promise-weather

---
+++ promise-weather/app.js
@@ -0,0 +1,20 @@
+const yargs = require('yargs');
+const axios = require('axios');
+
+const argv = yargs.options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: "Address",
+        string: true
+    }
+}).help().alias('help','h').argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+axios.get(geocodeUrl).then((response) => {
+    console.log(response.data);
+}).catch((err) => {
+    console.log(err);
+});
